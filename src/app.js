735840b430bdfa85f32b79261043aa99114d0749
aliciaelena <alicia.nagele@gmx.at>
2021-12-27 20:04:56 +0100

    Integrated current location weather with API

--- src/app.js
+++ src/app.js
@@ -115,8 +115,24 @@ function showForecast(response) {
   forecast.innerHTML = forecastHTML;
 }
 
+function handlePosition(location) {
+  let lon = location.coords.longitude;
+  let lat = location.coords.latitude;
+  let apiKey = "6a119ce5ad60b9883a83a56308bcd89c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", handleSubmit);
+
+let currentLocationButton = document.querySelector("#currentLocationButton");
+currentLocationButton.addEventListener("click", getCurrentPosition);
 
 let celciusDegrees = null;
 
